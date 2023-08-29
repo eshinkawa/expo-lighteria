fe010a81b9bb676c7b8b4be34fc8cf7047e2cad7
Eduardo Shinkawa <eshinkawa@gmail.com>
2023-08-29 08:51:55 -0300

    Commit1-Aula6

---
+++ app/screens/ListaDesejos/index.js
@@ -0,0 +1,14 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View, Text } from "react-native";
+
+export default function ListaDesejos() {
+  // puxar os dados do AsyncStorage
+  // mostrar os dados na tela em forma de lista (FlatList)
+  // criar um componente separado para renderizar os itens da lista (itemLista.js)
+
+  return (
+    <View>
+      <Text>ListaDesejos</Text>
+    </View>
+  );
+}
